a342593330983774ed4d0d6effde40f9519289dc
Marc Anthony Reyes <marcreyesmedia@gmail.com>
2018-04-21 23:08:10 +0800

    Add import image from a folder

---
+++ process_image.py
@@ -0,0 +1,16 @@
+import os
+import matplotlib.pyplot as plt 
+
+
+'''Import images from folder_name that ends with .jpg'''
+def import_image_files():
+	plants_images = [os.path.join(folder_name, img_i)
+	                 for img_i in os.listdir(folder_name)
+	                 if img_i.endswith('.jpg')]
+	return plants_images
+
+'''Reads images for plotting'''
+def read_image_files(plant_images):
+	return [plt.imread(img_i) for img_i in plant_images]
+
+
