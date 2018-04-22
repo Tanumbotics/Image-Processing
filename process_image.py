6b3f9716a85edd88b68702bf51e90459bf231a47
Marc Anthony Reyes <marcreyesmedia@gmail.com>
2018-04-22 22:29:48 +0800

    Add crop image to square function

--- process_image.py
+++ process_image.py
@@ -2,6 +2,7 @@ import os
 # import matplotlib.pyplot as plt
 import cv2
 
+
 def count_images_in_dir(folder):
     return len([img_i
                 for img_i in os.listdir(folder)
@@ -20,8 +21,19 @@ def read_image_files(folder):
     return [cv2.imread(img_i) for img_i in import_image_files(folder)]
 
 
-def resize_images():
-    '''TODO: Make a function to resize all images in a dir into same size
-    In image processing ML, you need to make sure all imgs are of the same
-    sizes
-    '''
+def crop_to_square(image):
+	if image.shape[0] > image.shape[1]:
+        extra = (image.shape[0] - image.shape[1])
+        if extra % 2 == 0:
+            crop = image[extra // 2: -extra // 2, :]
+        else:
+            crop = image[max(0, extra // 2 + 1): min(-1, -(extra // 2)), :]
+    elif image.shape[1] > image.shape[0]:
+        extra = (image.shape[1] = image.shape[0])
+        if extra % 2 == 0:
+            crop = image[:, extra // 2, -extra // 2]
+        else:
+            crop = image[:, max(0, extra // 2 + 1): min(-1, -(extra // 2))]
+    else:
+        crop = image
+    return crop
