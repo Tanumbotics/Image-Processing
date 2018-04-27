1b8bd859e78457259435d3df1905acf5fe2de028
Marc Anthony Reyes <marcreyesmedia@gmail.com>
2018-04-27 18:28:35 +0800

    Workaround on failing VVI filter

--- vari_test.py
+++ vari_test.py
@@ -11,19 +11,15 @@ def main():
     plant_img = read_image_files(FOLDER_DIR)
 
     for i in range(count_images_in_dir(FOLDER_DIR)):
-        img_rgb = imcrop_tosquare(plant_img[i])
+        img_rgb = plant_img[i]
         b, g, r = cv2.split(img_rgb)
-        b_0 = 0
-        r_0 = 30
-        g_0 = 50
 
-        vvi_0 = 1 - ((r - r_0) / (r + r_0))
-        vv1_1 = 1 - ((g - g_0) / (g + g_0))
-        vv1_2 = 1 - ((b - b_0) / (b + b_0))
+        r_0, g_0, b_0 = [1000, 100, 1000]
+
+        vvi_0 = 1 - (np.absolute(np.divide(np.subtract(r, r_0), np.add(r, r_0))))
+        vv1_1 = 1 - (np.absolute(np.divide(np.subtract(g, g_0), np.add(g, g_0))))
+        vv1_2 = 1 - (np.absolute(np.divide(np.subtract(b, b_0), np.add(b, b_0))))
         vvi = np.multiply(vvi_0, vv1_1, vv1_2)
-        # red_channel = (r / (r + g + b))
-        # green_chanel = (g / (r + g + b))
-        # blue_channel = (b / (r + g + b))
 
         iso_green = ((2 * g) + b - (2 * r))
 
@@ -38,14 +34,9 @@ def main():
         print('Preparing to process:\t' + g_file)
         cv2.imwrite(os.path.join(g_export_dir, g_file), iso_green)
         print('Preparing to process:\t' + v_file)
-        # cv2.imwrite(os.path.join(v_export_dir, v_file), vvi)
+        cv2.imwrite(os.path.join(v_export_dir, v_file), vvi)
         print('Done processing:\t' + v_file)
     print('Done procesing image in\t' + FOLDER_DIR)
-    print(r, g, b)
-    print(r.shape, g.shape, b.shape)
-    print(r_0, g_0, b_0)
-    # print(r_0.shape, g_0.shape, b_0.shape)
-    print(vvi)
 
 if __name__ == "__main__":
     main()
