29003798721f81db8a8642ee86fb010a0598845f
Marc Anthony Reyes <marcreyesmedia@gmail.com>
2018-04-27 15:41:08 +0800

    Extract green from image

--- vari_test.py
+++ vari_test.py
@@ -11,42 +11,33 @@ def main():
     plant_img = read_image_files(FOLDER_DIR)
 
     for i in range(count_images_in_dir(FOLDER_DIR)):
-        img = imcrop_tosquare(plant_img[i])
-        b, g, r = cv2.split(img)
+        img_rgb = imcrop_tosquare(plant_img[i])
+        b, g, r = cv2.split(img_rgb)
+        # red_channel = (r / (r + g + b))
+        # green_chanel = (g / (r + g + b))
+        # blue_channel = (b / (r + g + b))
+        iso_green = ((2 * g) + b - (2 * r))
 
         # Specify filename for each channel e.g. blue: b_00001.jpg
-        b_file = 'b_000%02d.jpg' % i
         g_file = 'g_000%02d.jpg' % i
-        r_file = 'r_000%02d.jpg' % i
-        v_file = 'v_000%02d.jpg' % i
 
         # Specify which directory to save processed channel
-        b_export_dir = 'exports/blue'
         g_export_dir = 'exports/green'
-        r_export_dir = 'exports/red'
-        v_export_dir = 'exports/vari'
 
-        # Write processed images
-        print('Preparing to process:\t' + b_file)
-        cv2.imwrite(os.path.join(b_export_dir, b_file), b)
-        print('Done processing:\t' + b_file)
         print('Preparing to process:\t' + g_file)
-        cv2.imwrite(os.path.join(g_export_dir, g_file), g)
+        cv2.imwrite(os.path.join(g_export_dir, g_file), iso_green)
         print('Done processing:\t' + g_file)
-        print('Preparing to process:\t' + r_file)
-        cv2.imwrite(os.path.join(r_export_dir, r_file), r)
-        print('Done processing:\t' + r_file)
-
-        '''VARI formula is green - red	 / green + red - blue
-		Source:http://www.harrisgeospatial.com/docs/BroadbandGreenness.html
-		To do Merge back: img = cv2.merge((b,g,r))
-		'''
-        print('Preparing to process:\t' +v_file)
-        VARI = g - r / g + r - b
-        cv2.imwrite(os.path.join(v_export_dir, v_file), VARI)
-        print('Done processing:\t' + r_file)
     print('Done procesing image in\t' + FOLDER_DIR)
 
+'''def split_image(image):
+    # Formula based from https://www.researchgate.net/post/Extract_Greenness2
+    b, g, r = cv2.split(image)
+    red_channel = (r / (r + g + b))
+    green_chanel = (g / (r + g + b))
+    blue_channel = (b / (r + g + b))
+    iso_green = ((2 * g) + b - (2 * r))
+    return iso_green
+'''
 
 if __name__ == "__main__":
     main()
